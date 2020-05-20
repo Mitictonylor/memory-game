ab8a834d942471861bc718b7062d4bdf5ededdb0
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-20 17:32:30 +0100

    add the flip in css

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.memory-card');
 //when we click a card we gonna add a class flip to this card(the one clicked)
 
 function flipCard() {
+  // toggle is ;ike a switch if i click once it takes the flip class  from the css
+  // if i click again it goes back to the previous class, so it's like an on and off
   this.classList.toggle('flip');
 }
 // to all the cards add this event listener
